328151b77467b19551926b53c8d11f505b8cf6bf
James Conway <ire.ulysses@gmail.com>
2020-11-23 09:32:08 +0100

    First-push

---
+++ main.js
@@ -0,0 +1,45 @@
+alert (
+`Hansel!!!
+Oh great, Gretels done it again. She's ratted you out to than new beast of a Stepmother in the hope that she can lick the Lolipop landing again. Well you can't blame her. Gingerbread cottages do have their perks`)
+
+let run = prompt("You have two choices, go down and apologise. Or cackle and run")
+
+if (run == "run") { alert ("Good choice")
+    
+} else {    alert ("Well that seemed to work, you should get some peace now....until the next time you go messing in Gretels Garderobe")
+    
+}
+
+let hide = prompt(`You've really pissed not mommy off now, better find a hiding spot and quickly. What will you do 
+(run) out into the woods , (lock)yourself in the bathroom or (hide) in Gretel's Garderobe`)
+
+/* if (hide == "hide") { alert("Hahaha, the silly cow ran straight past. Maybe she'll trip on something")
+    
+} 
+
+if (hide == "lock") { alert("Phew, peace at last. Oh-no! The lock's turning, She's found me")
+    
+}
+
+if (hide == "run") */
+/* 
+if (hide == "run") { let backInside = prompt("what will you do now? run around the house in the hopes that she'll follow you outside allowing you to get back (inside) or (keep-running)")}
+
+let backInside = "inside"
+
+if (backInside == "inside") { let hide = prompt("Phew, back in the warm now. What will you do now (lock)yourself in the bathroom or (hide) in Gretel's Garderobe ")
+    
+} else { alert("Splat! You've landed face down in the snow. and to top it off you've ruined Gretel's fave heels. She's gonna kill you")
+    
+} */
+
+let inside = prompt("what will you do now? run around the house in the hopes that she'll follow you outside allowing you to get back (inside) or (keep-running)")
+if (inside == "inside") { alert("Phew, back in the warm now. It wont be long until the heifer gets back in. better loook for a hiding place")
+    
+} else {
+    alert("Splat! You've landed face down in the snow. and to top it off you've ruined Gretel's fave heels. She's gonna kill you")
+}
+
+/*
+So, what I want to do is have the story progress then when the player puts in a variable the story goes back to the start
+*/
